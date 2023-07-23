c44554cf708e48cf47efd397e5a4f740e8eac8a7
siddhnigam-deloitte <svinayakamnigam@deloitte.com>
2023-07-23 19:17:57 +0530

    Db Connections

--- Db/Db.js
+++ Db/Db.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('final', 'postgres', 'postgres', {
-    host: 'localhost',
+const sequelize = new Sequelize('nodeassignment', 'nodeassignment_user', 'CWlmiNIhlMANBkfOruqApVx92WGYPoxo', {
+    host: 'dpg-ciuic4liuiedpv092sh0-a',
     dialect: 'postgres' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 });
 
